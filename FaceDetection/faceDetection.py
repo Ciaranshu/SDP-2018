b03e9a8b1148204105e5e86b6874d2d98a075cc1
Jason Yin <zichenyin25@gmail.com>
2018-02-13 17:14:58 +0000

    track biggest face

--- FaceDetection/faceDetection.py
+++ FaceDetection/faceDetection.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import time
 
 face_cascade =cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Don't need
@@ -11,37 +10,49 @@ right_border = 500
 face_max = 350
 face_min = 280
 
-def FaceBorder(x):
-    if(x > left_border):
+# Detect the borders
+def FaceBorder(c_x):
+    if(c_x > left_border):
         # Rotate robot to left
         print("out of left border")
-    if(x < right_border):
+    if(c_x < right_border):
         # Rotate robot to right
         print("out of right border")
 
 while True:
-    # Reduce size
-    # time.sleep(2)
+    biggestFace = 0
+    _x = 0
+    _y = 0
+    _w = 0
+    _h = 0
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 3)
-    # first_face = faces[0]
+
     # print("Found %s faces!" % len(faces))
     # if len(faces) != 0:
     #     print("FACE")
     # else:
     #     print("NO FACE")
     for(x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-        centre_x = x + w/2
+        if (w*h > biggestFace):
+            _x = x
+            _y = y
+            _w = w
+            _h = h
+            biggestFace = _w*_h
+
+    if (biggestFace > 0):
+        cv2.rectangle(img, (_x,_y), (_x+_w, _y+_h), (255,0,0), 2)
+        centre_x = _x + _w/2
         # centre_y = y + y/2
         # print("X is %i, Y is %i, W is %i, H is %i" % (x, y, w, h))
 
         # check if face is out of the border
-        if(h > face_max):
+        if(_h > face_max):
             # move robot back
             print("face too big")
-        elif(h < face_min):
+        elif(_h < face_min):
             # move robot forward
             print("face too small")
         else:
