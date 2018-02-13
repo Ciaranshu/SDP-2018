7ef8706a9343a07c73e4d8454474c90b80deaa09
Jason Yin <zichenyin25@gmail.com>
2018-02-13 17:17:51 +0000

    clear the code

--- FaceDetection/faceDetection.py
+++ FaceDetection/faceDetection.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 
 face_cascade =cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Don't need
-# eye_cascade =cv2.CascadeClassifier('haarcascade_eye.xml')
 cap = cv2.VideoCapture(0)
 left_border = 700
 right_border = 500
@@ -11,11 +9,11 @@ face_max = 350
 face_min = 280
 
 # Detect the borders
-def FaceBorder(c_x):
-    if(c_x > left_border):
+def FaceBorder(face_centre):
+    if(face_centre > left_border):
         # Rotate robot to left
         print("out of left border")
-    if(c_x < right_border):
+    if(face_centre < right_border):
         # Rotate robot to right
         print("out of right border")
 
@@ -29,11 +27,7 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 3)
 
-    # print("Found %s faces!" % len(faces))
-    # if len(faces) != 0:
-    #     print("FACE")
-    # else:
-    #     print("NO FACE")
+    # track the closest face
     for(x,y,w,h) in faces:
         if (w*h > biggestFace):
             _x = x
@@ -42,11 +36,10 @@ while True:
             _h = h
             biggestFace = _w*_h
 
+    # draw rectangle on closest face
     if (biggestFace > 0):
         cv2.rectangle(img, (_x,_y), (_x+_w, _y+_h), (255,0,0), 2)
         centre_x = _x + _w/2
-        # centre_y = y + y/2
-        # print("X is %i, Y is %i, W is %i, H is %i" % (x, y, w, h))
 
         # check if face is out of the border
         if(_h > face_max):
@@ -57,13 +50,6 @@ while True:
             print("face too small")
         else:
             FaceBorder(centre_x)
-
-        # print("h is %i" %(h))
-        # roi_gray= gray[y:y+h, x:x+w]
-        # roi_color = img[y:y+h, x:x+w]
-        # eyes = eye_cascade.detectMultiScale(roi_gray)
-        # for(ex,ey,ew,eh) in eyes:
-        #     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+ew), (0,255,0), 2)
 
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
