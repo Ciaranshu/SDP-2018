8cea85272ef6289b229de535e9eb1c6194221f3c
Akshay <s1558717@sms.ed.ac.uk>
2018-02-26 21:58:10 +0000

    All app changes

--- rpiconnect.py
+++ rpiconnect.py
@@ -1,8 +1,12 @@
 from bluetooth import *
+import paho.mqtt.client as mqtt
 
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
+
+#client = mqtt.Client()
+#client.connect("10.42.0.54", 1883, 60)
 
 port = server_sock.getsockname()[1]
 
@@ -12,33 +16,34 @@ advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
                    profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
+#                  protocols = [ OBEX_UUID ]
                     )
-                   
-print("Waiting for connection on RFCOMM channel %d" % port)
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+while True:
+    print("Waiting for connection on RFCOMM channel %d" % port)
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
+    try:
+        data = client_sock.recv(1024).decode()
+        if len(data) == 0:
+            break
         print("received [%s]" % data)
 
 		if data == 'game1':
 			data = 'Playing game 1!'
-		elif data == 'runWheels':
+            #client.publish("topic/motor-A/dt", "1");
+		elif data == 'runWheels':,
 			data = 'Playing game 2!'
 		elif data == 'game3':
 			data = 'Playing game 3!'
 		else:
 			data = 'Playing game 4!' 
-	        client_sock.send(data)
+        client_sock.send(data)
 		print "sending [%s]" % data
 
-except IOError:
-    pass
+    except IOError:
+        pass
 
 print("disconnected")
 
