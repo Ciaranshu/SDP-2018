047e7cbcd5f0fd5af60d4bd06290695d23bab039
ciaran <cscshu@outlook.com>
2018-03-13 21:28:46 +0000

    Add sidebar, viewpager and alterDialog

--- App/app/src/main/java/com/example/stelios/leoappjavamosquitto/MainActivity.java
+++ App/app/src/main/java/com/example/stelios/leoappjavamosquitto/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.stelios.leoappjavamosquitto;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Message;
 import android.os.ParcelUuid;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,9 +40,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -38,7 +55,7 @@ import com.parse.SaveCallback;
 
 import okhttp3.internal.Util;
 
-public class MainActivity extends AppCompatActivity implements org.eclipse.paho.client.mqttv3.MqttCallback {
+public class MainActivity extends AppCompatActivity implements org.eclipse.paho.client.mqttv3.MqttCallback, NavigationView.OnNavigationItemSelectedListener {
 
     //    private OutputStream outputStream;
     private InputStream inStream;
@@ -52,24 +69,182 @@ public class MainActivity extends AppCompatActivity implements org.eclipse.paho.
     private MqttMessage msgCancel;
     private ArrayList<String> lista;
     private MqttConnectOptions timeOut;
+    private boolean flag;
 
+    // viewpager code
+    private View view1, view2;
+    private ViewPager viewPager;
+    private List<View> viewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_side);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+//        View decorView = getWindow().getDecorView();
+//        // Hide the status bar.
+//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
 
-        Parse.initialize(this);
-        ParseInstallation.getCurrentInstallation().saveInBackground();
+        // Viewpager code
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        LayoutInflater inflater=getLayoutInflater();
+        view1 = inflater.inflate(R.layout.content_main_activity_side, null);
+        view2 = inflater.inflate(R.layout.activity_long_game_graph,null);
+
+        viewList = new ArrayList<View>();// 将要分页显示的View装入数组中
+        viewList.add(view1);
+        viewList.add(view2);
+
+
+        PagerAdapter pagerAdapter = new PagerAdapter() {
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                // TODO Auto-generated method stub
+                return arg0 == arg1;
+            }
+
+            @Override
+            public int getCount() {
+                // TODO Auto-generated method stub
+                return viewList.size();
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position,
+                                    Object object) {
+                // TODO Auto-generated method stub
+                container.removeView(viewList.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                // TODO Auto-generated method stub
+                container.addView(viewList.get(position));
+
+
+                return viewList.get(position);
+            }
+        };
+
+        viewPager.setAdapter(pagerAdapter);
+
+
+
+
+        // end of viewpager
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        MaterialDialog.Builder dialogBuilder = new MaterialDialog.Builder(this)
+                .title(R.string.title)
+                .content(R.string.content)
+                .positiveText(R.string.agree)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(MaterialDialog dialog, DialogAction which) {
+                        dialog.dismiss();
+                        //createConnection();
+
+
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(MaterialDialog dialog, DialogAction which) {
+                        finish();
+                    }
+                });
+
+        final MaterialDialog connectionDialog = dialogBuilder.build();
+        connectionDialog.show();
 
     }
 
-    public void createConnection(View view) {
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_side, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public boolean createConnection() {
 
         // Setting timeout Interval for connection
         timeOut = new MqttConnectOptions();
-        timeOut.setKeepAliveInterval(600000);
+        timeOut.setKeepAliveInterval(600);
 
         try {
             //"tcp://10.42.0.1:1883"
@@ -110,9 +285,18 @@ public class MainActivity extends AppCompatActivity implements org.eclipse.paho.
             int duration = Toast.LENGTH_SHORT;
             Toast.makeText(context, text, duration).show();
 
+            flag = true;
+            return flag;
+
         } catch (MqttException e) {
             e.printStackTrace();
+            Context context = getApplicationContext();
+            CharSequence text = "Did not connect!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+            return flag;
         }
+
     }
 
     public void StartGameOne(View view) {
@@ -178,6 +362,7 @@ public class MainActivity extends AppCompatActivity implements org.eclipse.paho.
         CharSequence text = "Connection terminated!";
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
+
     }
 
     @Override
@@ -187,6 +372,9 @@ public class MainActivity extends AppCompatActivity implements org.eclipse.paho.
         CharSequence text = "Connection Lost!";
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
+
+        flag = false;
+
 
     }
 
