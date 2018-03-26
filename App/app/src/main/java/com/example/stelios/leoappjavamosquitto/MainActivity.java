129b4d398cd5ad315909388a573b11907c69b87e
Jason Yin <zichenyin25@gmail.com>
2018-03-26 13:40:42 +0100

    update login UI

--- App/app/src/main/java/com/example/stelios/leoappjavamosquitto/MainActivity.java
+++ App/app/src/main/java/com/example/stelios/leoappjavamosquitto/MainActivity.java
@@ -4,56 +4,39 @@ package com.example.stelios.leoappjavamosquitto;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Message;
-import android.os.ParcelUuid;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
 import com.parse.ParseInstallation;
 import com.parse.SaveCallback;
-
-import okhttp3.internal.Util;
 
 public class MainActivity extends AppCompatActivity implements org.eclipse.paho.client.mqttv3.MqttCallback, NavigationView.OnNavigationItemSelectedListener {
 
@@ -80,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements org.eclipse.paho.
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_side);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         lista = new ArrayList<String>();
         Parse.initialize(this);
@@ -90,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements org.eclipse.paho.
         // Viewpager code
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         LayoutInflater inflater=getLayoutInflater();
-        view1 = inflater.inflate(R.layout.content_main_activity_side, null);
+        view1 = inflater.inflate(R.layout.basic_game_layout, null);
         view2 = inflater.inflate(R.layout.memory_game_layout,null);
         view3 = inflater.inflate(R.layout.reaction_game_layout,null);
         view4 = inflater.inflate(R.layout.face_detection_layout,null);
